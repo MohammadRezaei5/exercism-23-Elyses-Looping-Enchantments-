1bb1fe81e68ac18a4ecd716dd2eea23ea9a0e546
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-11 10:03:43 +0330

    Function: determineOddEvenCards

--- enchantments.js
+++ enchantments.js
@@ -11,3 +11,21 @@ function cardTypeCheck(stack, card) {
 }
 
 // console.log(cardTypeCheck([1, 2, 3, 4], cardType));
+
+///---///
+
+function determineOddEvenCards(stack, type) {
+  const rest = type ? 0 : 1;
+
+  let count = 0;
+
+  for (const card of stack) {
+    if (card % 2 === rest) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+// console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], true));
+// console.log(determineOddEvenCards([1, 2, 3, 1, 5, 6], false));
