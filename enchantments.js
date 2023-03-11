edf018a3291754c601d9b36d899f1b5730aa0e92
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-11 10:03:16 +0330

    Function: cardTypeCheck

--- enchantments.js
+++ enchantments.js
@@ -1,1 +1,13 @@
 // look back again and again, read more ...
+
+// const cardType = 3;
+
+function cardTypeCheck(stack, card) {
+  let count = 0;
+  stack.forEach((needle) => {
+    count += needle === card ? 1 : 0;
+  });
+  return count;
+}
+
+// console.log(cardTypeCheck([1, 2, 3, 4], cardType));
